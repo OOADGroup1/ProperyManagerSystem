c00b53992ac936e09817ef18cab4e611b4507d49
YingShen <yingshen94@gmail.com>
2016-12-30 14:49:59 +0800

    ItemManager func

--- src/com/propertysys/user/ItemManager.java
+++ src/com/propertysys/user/ItemManager.java
@@ -1,29 +1,50 @@
 package com.propertysys.user;
 
-import com.propertysys.bean.EquipItemBean;
-import com.propertysys.bean.SpareItemBean;
+import com.propertysys.bean.*;
 import com.propertysys.operation.EquipItemOperator;
+import com.propertysys.operation.EquipManageRecordOperator;
 import com.propertysys.operation.SpareItemOperator;
-import com.sun.tools.javac.util.List;
+import com.propertysys.operation.SpareManageRecordOperator;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import java.sql.Timestamp;
 
 /**
  * Created by Sunny on 16/12/28.
  */
 public class ItemManager {
 
+    private final String BUY = "buy";
+    private final String DISCARD = "discard";
+    private ItemManagerBean itemManagerBean;
     private EquipItemOperator equipItemOperator;
+    private EquipManageRecordOperator equipManageRecordOperator;
     private SpareItemOperator spareItemOperator;
+    private SpareManageRecordOperator spareManageRecordOperator;
 
-    public ItemManager(){
+    public ItemManager(ItemManagerBean itemManagerBean){
+        this.itemManagerBean = itemManagerBean;
         equipItemOperator = new EquipItemOperator();
+        equipManageRecordOperator = new EquipManageRecordOperator();
         spareItemOperator = new SpareItemOperator();
+        spareManageRecordOperator = new SpareManageRecordOperator();
     }
     /**
      * buy equipment
      * @param equipItem
      */
     private void buyEquipItem(EquipItemBean equipItem){
+        // add equipItem
         equipItemOperator.insert(equipItem);
+        // add equipManageRecord
+        EquipManageRecordBean equipManageRecordBean = new EquipManageRecordBean();
+        equipManageRecordBean.setEquipSeriesId(equipItem.getEquipSeriesId());
+        equipManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+        equipManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+        equipManageRecordBean.setManageType(BUY);
+        equipManageRecordOperator.insert(equipManageRecordBean);
     }
 
     /**
@@ -31,7 +52,20 @@ public class ItemManager {
      * @param equipItemList
      */
     private void buyEquipItems(List<EquipItemBean> equipItemList){
+        // add equipment list
         equipItemOperator.insertAll(equipItemList);
+        // add record list
+        List<EquipManageRecordBean> buyList = new ArrayList<EquipManageRecordBean>();
+        for(Iterator iter = equipItemList.iterator(); iter.hasNext();) {
+            EquipItemBean equipItemBean = (EquipItemBean) iter.next();
+            EquipManageRecordBean equipManageRecordBean = new EquipManageRecordBean();
+            equipManageRecordBean.setEquipSeriesId(equipItemBean.getEquipSeriesId());
+            equipManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+            equipManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+            equipManageRecordBean.setManageType(BUY);
+            buyList.add(equipManageRecordBean);
+        }
+        equipManageRecordOperator.insertAll(buyList);
     }
 
     /**
@@ -39,7 +73,15 @@ public class ItemManager {
      * @param spareItem
      */
     private void buySpareItem(SpareItemBean spareItem){
+        // add spareItem
         spareItemOperator.insert(spareItem);
+        // add SpareManageRecord
+        SpareManageRecordBean spareManageRecordBean = new SpareManageRecordBean();
+        spareManageRecordBean.setSpareSeriesId(spareItem.getSpareSeriesId());
+        spareManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+        spareManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+        spareManageRecordBean.setManageType(BUY);
+        spareManageRecordOperator.insert(spareManageRecordBean);
     }
 
     /**
@@ -47,7 +89,20 @@ public class ItemManager {
      * @param spareItemList
      */
     private void buySpareItems(List<SpareItemBean> spareItemList){
+        // add spare list
         spareItemOperator.insertAll(spareItemList);
+        // add record list
+        List<SpareManageRecordBean> buyList = new ArrayList<SpareManageRecordBean>();
+        for(Iterator iter = spareItemList.iterator(); iter.hasNext();) {
+            SpareItemBean spareItemBean = (SpareItemBean) iter.next();
+            SpareManageRecordBean spareManageRecordBean = new SpareManageRecordBean();
+            spareManageRecordBean.setSpareSeriesId(spareItemBean.getSpareSeriesId());
+            spareManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+            spareManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+            spareManageRecordBean.setManageType(BUY);
+            buyList.add(spareManageRecordBean);
+        }
+        spareManageRecordOperator.insertAll(buyList);
     }
 
     /**
@@ -55,7 +110,15 @@ public class ItemManager {
      * @param equipId
      */
     private void discardEquipById(int equipId){
+        // delete equip from equipItem table
         equipItemOperator.delete(equipId);
+        // add discard record
+        EquipManageRecordBean equipManageRecordBean = new EquipManageRecordBean();
+        equipManageRecordBean.setEquipSeriesId(equipId);
+        equipManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+        equipManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+        equipManageRecordBean.setManageType(DISCARD);
+        equipManageRecordOperator.insert(equipManageRecordBean);
     }
 
     /**
@@ -63,7 +126,15 @@ public class ItemManager {
      * @param equipItem
      */
     private void discardEquip(EquipItemBean equipItem){
+        // delete equip from equipItem table
         equipItemOperator.delete(equipItem);
+        // add discard record
+        EquipManageRecordBean equipManageRecordBean = new EquipManageRecordBean();
+        equipManageRecordBean.setEquipSeriesId(equipItem.getEquipSeriesId());
+        equipManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+        equipManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+        equipManageRecordBean.setManageType(DISCARD);
+        equipManageRecordOperator.insert(equipManageRecordBean);
     }
 
     /**
@@ -71,7 +142,15 @@ public class ItemManager {
      * @param spareId
      */
     private void discardSpareById(int spareId){
+        // delete spare from spareItem table
         spareItemOperator.delete(spareId);
+        // add discard record
+        SpareManageRecordBean spareManageRecordBean = new SpareManageRecordBean();
+        spareManageRecordBean.setSpareSeriesId(spareId);
+        spareManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+        spareManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+        spareManageRecordBean.setManageType(DISCARD);
+        spareManageRecordOperator.insert(spareManageRecordBean);
     }
 
     /**
@@ -79,7 +158,15 @@ public class ItemManager {
      * @param spareItem
      */
     private void dicardSpare(SpareItemBean spareItem){
+        // delete spare from spareItem table
         spareItemOperator.delete(spareItem);
+        // add discard record
+        SpareManageRecordBean spareManageRecordBean = new SpareManageRecordBean();
+        spareManageRecordBean.setSpareSeriesId(spareItem.getSpareSeriesId());
+        spareManageRecordBean.setManagerId(itemManagerBean.getManagerId());
+        spareManageRecordBean.setManageDate(new Timestamp(System.currentTimeMillis()));
+        spareManageRecordBean.setManageType(DISCARD);
+        spareManageRecordOperator.insert(spareManageRecordBean);
     }
 
 
