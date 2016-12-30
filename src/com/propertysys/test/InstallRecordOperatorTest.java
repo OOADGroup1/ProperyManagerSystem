e28ac2611e58b4956abb284a665a024daafd5320
YingShen <yingshen94@gmail.com>
2016-12-31 01:22:00 +0800

    install test

---
+++ src/com/propertysys/test/InstallRecordOperatorTest.java
@@ -0,0 +1,34 @@
+package com.propertysys.test;
+
+import com.propertysys.bean.InstallRecordBean;
+import com.propertysys.operation.InstallRecordOperator;
+import org.junit.Before;
+import org.junit.Test;
+import org.omg.CORBA.Object;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shenying on 16/12/31.
+ */
+public class InstallRecordOperatorTest {
+
+    private InstallRecordOperator installRecordOperator;
+
+    @Before
+    public void setUp() throws Exception {
+        installRecordOperator = new InstallRecordOperator();
+    }
+
+    @Test
+    public void viewInstallRecByEquipId() throws Exception {
+        List<InstallRecordBean> list = installRecordOperator.viewInstallRecByEquipId(20160102);
+        for (InstallRecordBean installRec:
+                list) {
+            System.out.println(installRec.getInstallDate() +
+                    " installed Spare(SpareSeriesId: " + installRec.getSpareSeriesId() + ")");
+        }
+    }
+}
