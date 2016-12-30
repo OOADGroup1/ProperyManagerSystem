a90be58eff5c1404e67e18ee1c1a22d33dfda70d
YingShen <yingshen94@gmail.com>
2016-12-30 16:06:49 +0800

    pass test equipment func: queryByPk

--- src/com/propertysys/operation/EquipmentOperator.java
+++ src/com/propertysys/operation/EquipmentOperator.java
@@ -1,9 +1,34 @@
 package com.propertysys.operation;
 
 import com.propertysys.bean.EquipmentBean;
+import com.propertysys.bean.EquipmentBeanPK;
+import com.propertysys.hibernate.HibernateUtils;
+import org.hibernate.HibernateException;
 
 /**
  * Created by Sunny on 16/12/28.
  */
 public class EquipmentOperator extends BaseOperator<EquipmentBean> implements IEquipmentOperator {
+
+    //    private Session session = this.session;
+//    private Transaction tx = this.tx;
+
+
+    public EquipmentBean queryByPK(EquipmentBeanPK pk){
+        EquipmentBean equipmentBean = null;
+        try {
+            startOperation();
+            equipmentBean = (EquipmentBean) session.get(EquipmentBean.class, pk);
+            tx.commit();
+        } catch (HibernateException e){
+            e.printStackTrace();
+        } finally {
+            HibernateUtils.closeSession(session);
+        }
+        return equipmentBean;
+    }
+
+    public void deleteByPK(EquipmentBeanPK pk){
+
+    }
 }
--- src/com/propertysys/operation/IEquipmentOperator.java
+++ src/com/propertysys/operation/IEquipmentOperator.java
@@ -1,9 +1,15 @@
 package com.propertysys.operation;
 
 import com.propertysys.bean.EquipmentBean;
+import com.propertysys.bean.EquipmentBeanPK;
+
+import java.util.List;
 
 /**
  * Created by shenying on 16/12/29.
  */
 public interface IEquipmentOperator extends IBaseOperator<EquipmentBean> {
+
+    EquipmentBean queryByPK(EquipmentBeanPK pk);
+    void deleteByPK(EquipmentBeanPK pk);
 }
