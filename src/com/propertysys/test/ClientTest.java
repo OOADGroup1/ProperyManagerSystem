b7d654ed5a3631c87b93ccde6990b941faa250d4
YingShen <yingshen94@gmail.com>
2016-12-31 00:12:44 +0800

    Client test

---
+++ src/com/propertysys/test/ClientTest.java
@@ -0,0 +1,55 @@
+package com.propertysys.test;
+
+import com.propertysys.user.Client;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shenying on 16/12/30.
+ */
+public class ClientTest {
+
+    private Client client;
+
+    @Before
+    public void setUp() throws Exception {
+        client = new Client();
+    }
+
+    @Test
+    public void viewAllEquips() throws Exception {
+
+    }
+
+    @Test
+    public void viewAllSpares() throws Exception {
+
+    }
+
+    @Test
+    public void viewAllRentEquipsByEmployeeId() throws Exception {
+        client.viewAllRentEquipsByEmployeeId(0);
+    }
+
+    @Test
+    public void viewAllRentSparesByEmployeeId() throws Exception {
+
+    }
+
+    @Test
+    public void viewEquipLifeCycleById() throws Exception {
+
+    }
+
+    @Test
+    public void viewSpareLifeCycleById() throws Exception {
+
+    }
+
+    @Test
+    public void viewEquipInstallRecoById() throws Exception {
+
+    }
+}
